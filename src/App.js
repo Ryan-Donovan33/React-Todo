20efdd4136f81fdf26f1a9b705b6139597b24816
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-11 21:31:11 -0500

    setting up constructor in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+const todos = [
+	{
+		task: 'Organize Garage',
+		id: 1528817077286,
+		completed: false
+	},
+	{
+		task: 'Bake Cookies',
+		id: 1528817084358,
+		completed: false
+	}
+];
+
+class App extends Component {
 	// you will need a place to store your state in this component.
 	// design `App` to be the parent component of your application.
 	// this component is going to take care of state, and any change handlers you need to work with your state
+	constructor() {
+		super();
+		this.state = {
+			todos
+		};
+	}
+
 	render() {
 		return (
-			<div>
-				<h2>Update App!</h2>
+			<div className="App">
+				<h2>ToDo List!</h2>
 			</div>
 		);
 	}
